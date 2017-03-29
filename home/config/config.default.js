fc243f7f0c4fdeae9271d6b1ad38eeaeacec5c39
束蓬 shupeng (99999) <shupeng@tuniu-inc.com>
2017-03-29 17:41:14 +0800

    'init'

---
+++ home/config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1490780246585_1274';
+
+  return config;
+};
